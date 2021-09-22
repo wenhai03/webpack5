c6922b4478c4b8efe7a29e510e543b17fb24bf3f
wenhai03 <810068733@qq.com>
2021-09-23 00:06:24 +0800

    init

---
+++ 01_初始化/src/index.js
@@ -0,0 +1,10 @@
+import { sum, mul } from './js/math.js';
+const { dateFormate, priceFormate } = require('./js/formate.js');
+
+
+
+console.log(sum(20, 30));
+console.log(mul(20, 30));
+
+console.log(dateFormate(2020));
+console.log(priceFormate(100))
