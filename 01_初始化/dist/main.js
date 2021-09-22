1224152eac7069accdbb692b99e9166684444c20
wenhai03 <810068733@qq.com>
2021-09-23 00:09:06 +0800

    init

---
+++ 01_初始化/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{var o={64:o=>{o.exports={dateFormate:o=>"2020-12-12",priceFormate:o=>"100.00"}}},e={};function r(t){var s=e[t];if(void 0!==s)return s.exports;var a=e[t]={exports:{}};return o[t](a,a.exports,r),a.exports}(()=>{"use strict";const{dateFormate:o,priceFormate:e}=r(64);console.log(50),console.log(600),console.log(o(2020)),console.log(e(100))})()})();
